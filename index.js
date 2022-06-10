db740413b0fbe52b3ac2204c9dcc9481213b399d
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-06-10 16:32:26 +0600

    ridwan suhel server side added with project collection

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.btk16.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const projectCollection = client
+      .db("ridwan-suhel-portfolio")
+      .collection("projects");
+
+    app.get("/projects", async (req, res) => {
+      const query = {};
+      const cursor = projectCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
